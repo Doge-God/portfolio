c407917c9d5d394f9b90875aa7881d9733a60112
FZ <62817503+Doge-God@users.noreply.github.com>
2022-09-10 19:44:11 +1200

    feat: animated navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -15,11 +15,11 @@ function NavBar() {
       <div className='hidden h-screen sm:flex flex-col font-raleway text-center text-lg xl:text-xl font-semibold xl:font-bold caret-transparent'>
         <ul className= 'mt-6 mb-16 flex flex-col justify-evenly'>
           <li className=' my-6 xl:my-6 text-ui-back-space-blue hover:bg-ui-back-space-blue hover:text-ui-pastel-blue'>
-            <Link to="test" spy={true} smooth={true} duration={500} className=' py-2 xl:py-3 inline-block w-full h-full'>HOME</Link>
+            <Link data-testid='fullHome' to="test" spy={true} smooth={true} duration={500} className=' py-2 xl:py-3 inline-block w-full h-full'>HOME</Link>
           </li>
 
           <li className=' my-6 xl:my-7 text-ui-back-space-blue hover:bg-ui-back-space-blue hover:text-ui-pastel-blue'>
-            <Link to="test2" spy={true} smooth={true} duration={500} className=' py-2 xl:py-3 inline-block w-full h-full'>ABOUT</Link>
+            <Link data-testid='fullAbout' to="test2" spy={true} smooth={true} duration={500} className=' py-2 xl:py-3 inline-block w-full h-full'>ABOUT</Link>
           </li>
 
           <li className=' my-6 xl:my-7 text-ui-back-space-blue hover:bg-ui-back-space-blue hover:text-ui-pastel-blue'>
@@ -33,7 +33,7 @@ function NavBar() {
       </div>
 
       {/*MOBILE MENU BUTTON*/}
-      <div onClick={() => dispatch(toggleMenu())} className='block absolute top-0 left-0 z-10 sm:hidden'>
+      <div data-testid='menuButton' onClick={() => dispatch(toggleMenu())} className='block absolute top-0 left-0 z-10 sm:hidden'>
         {isMenuOn ? <AiOutlineClose size={40} color={'#202231'} className='m-6'/> :
           <AiOutlineMenu size={40} color={'#b0b8e8'} className='m-6' />
         }
@@ -41,22 +41,26 @@ function NavBar() {
       </div>
 
       {/*MOBILE MENU*/}
-      <div className=' bg-amber-600 h-screen w-28 top-0 left-0 justify-evenly absolute sm:hidden flex flex-col font-raleway text-center text-xl font-bold caret-transparent'>
+      <div className= {
+          isMenuOn ? ' ease-in duration-150 bg-ui-pastel-blue h-screen w-40 top-0 left-0 justify-evenly absolute sm:hidden flex flex-col font-raleway text-center text-xl font-bold caret-transparent'
+          :  'ease-in duration-150 bg-ui-pastel-blue h-screen w-40 top-0 -left-40 justify-evenly absolute sm:hidden flex flex-col font-raleway text-center text-xl font-bold caret-transparent'
+        }
+      >
         <ul className= ' mt-40 mb-16 flex flex-col justify-evenly'>
           <li className=' my-6 xl:my-6 text-ui-back-space-blue hover:bg-ui-back-space-blue hover:text-ui-pastel-blue'>
-            <Link to="test" spy={true} smooth={true} duration={500} className=' py-2 xl:py-3 inline-block w-full h-full'>HOME</Link>
+            <Link onClick={() => dispatch(toggleMenu())} to="test" spy={true} smooth={true} duration={500} className=' py-2 xl:py-3 inline-block w-full h-full'>HOME</Link>
           </li>
 
           <li className=' my-6 xl:my-7 text-ui-back-space-blue hover:bg-ui-back-space-blue hover:text-ui-pastel-blue'>
-            <Link to="test2" spy={true} smooth={true} duration={500} className=' py-2 xl:py-3 inline-block w-full h-full'>ABOUT</Link>
+            <Link onClick={() => dispatch(toggleMenu())} to="test2" spy={true} smooth={true} duration={500} className=' py-2 xl:py-3 inline-block w-full h-full'>ABOUT</Link>
           </li>
 
           <li className=' my-6 xl:my-7 text-ui-back-space-blue hover:bg-ui-back-space-blue hover:text-ui-pastel-blue'>
-            <Link to="test3" spy={true} smooth={true} duration={500} className=' py-2 xl:py-3 inline-block w-full h-full'>PROJ.</Link>
+            <Link onClick={() => dispatch(toggleMenu())} to="test3" spy={true} smooth={true} duration={500} className=' py-2 xl:py-3 inline-block w-full h-full'>PROJ.</Link>
           </li>
         </ul>
 
-        <Link to="test" spy={true} smooth={true} duration={500} className=' 
+        <Link onClick={() => dispatch(toggleMenu())} to="test" spy={true} smooth={true} duration={500} className=' 
          py-2 mt-auto mb-14 text-ui-back-space-blue hover:bg-ui-back-space-blue hover:text-ui-pastel-blue'>CONTACT</Link>
       </div>
       
